fda4694425323ee5a7e3e695699f3bee82ffc79f
1armgod <jp215053@falmouth.ac.uk>
2021-05-21 14:30:55 +0100

    Fine Tuned Level, Added UI Functionality to New Level Added Zorps to New Level Added Traps to New Level

---
+++ Zorp URP/Assets/new stuff/EnemyNewNew.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNewNew : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private NavMeshAgent nav;
+
+    public float health = 100f;
+
+    public bool Oil;
+    public bool fire;
+
+
+
+    private void Start()
+    {
+
+        target = Waypoints.points[0];
+        nav = this.GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        nav.SetDestination(dir.normalized * speed * Time.deltaTime);
+    }
+
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            this.gameObject.GetComponent<ExplodeZorp>().Explosion();
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+
+        Destroy(gameObject);
+    }
+}
